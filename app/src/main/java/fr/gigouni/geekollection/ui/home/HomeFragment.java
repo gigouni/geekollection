61fb73fdbabdde8b459391bc650f4decbaa71797
GIGOU <nicolas.gigou@gmail.com>
2019-12-30 03:22:44 +0100

    feature: add thumbnails from home fragment

--- app/src/main/java/fr/gigouni/geekollection/ui/home/HomeFragment.java
+++ app/src/main/java/fr/gigouni/geekollection/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package fr.gigouni.geekollection.ui.home;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -22,14 +25,80 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+        ImageView imgBritto = root.findViewById(R.id.britto);
+        imgBritto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new AlertDialog.Builder(getContext())
+                        .setTitle("BRITTO - Nothing yet")
+                        .setMessage("Feature coming soon...")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
             }
         });
+
+        ImageView imgPops = root.findViewById(R.id.pop);
+        imgPops.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new AlertDialog.Builder(getContext())
+                        .setTitle("FUNKO POPS - Nothing yet")
+                        .setMessage("Feature coming soon...")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
+
+        ImageView imgVideosGames = root.findViewById(R.id.video_games);
+        imgVideosGames.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new AlertDialog.Builder(getContext())
+                        .setTitle("VIDEO GAMES - Nothing yet")
+                        .setMessage("Feature coming soon...")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
+
+
         return root;
     }
 }
